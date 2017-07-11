a43b20ad52065cfb85950ea9fbf8a41acea66b42
Scott DeVito <scottadevito@gmail.com>
2017-07-10 23:18:45 -0400

    Render Belts into BeltList/call fetchBelts action on sucuessful mount

--- src/components/belt_list.js
+++ src/components/belt_list.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import Belts from './belts';
 
 class BeltList extends Component {
+    componentDidMount() {
+        this.props.fetchBelts();
+    }
+
     render() {
         return (
-            <div>
-                <p>BeltList</p>
+            <div className="belt-list">
+                <Belts belts={this.props.belts} selectBelt={this.props.selectBelt} />
             </div>
         );
     }
-}
+};
 
 export default BeltList;
