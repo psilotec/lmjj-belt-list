14b3b2aea15113ab36342c49070398dbe7446971
Scott DeVito <scottadevito@gmail.com>
2017-07-12 21:51:07 -0400

    Create BeltImagesReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import BeltsReducer from './reducer_belts';
 import SelectedBeltReducer from './reducer_selected_belt';
+import BeltImagesReducer from './reducer_belt_images';
 
 const rootReducer = combineReducers({
   belts: BeltsReducer,
   selectedBelt: SelectedBeltReducer,
+  beltImages: BeltImagesReducer,
 });
 
 export default rootReducer;
