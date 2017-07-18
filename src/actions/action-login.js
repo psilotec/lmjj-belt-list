92b75efca7fd3471d0e258b59ebd0a73ffeec8fd
Scott DeVito <scottadevito@gmail.com>
2017-07-17 21:17:54 -0400

    Create login action

---
+++ src/actions/action-login.js
@@ -0,0 +1,23 @@
+import { LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import database from '../startup/db';
+import firebase from 'firebase';
+
+const login = ({ email, password }) => {
+    return dispatch => {
+        return new Promise((resolve, reject) => {
+            firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((user) => {
+                dispatch({ type: LOGIN_SUCCESS, payload: { user } });
+                resolve(user);
+            })
+            .catch((error) => {
+                dispatch({ type: LOGIN_FAIL, payload: { error } });
+                reject(error);
+            });
+        });
+    };
+};
+
+export {
+    login,
+};
--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,7 @@
 export const FETCH_BELTS = 'FETCH_BELTS';
 export const BELT_SELECTED = 'BELT_SELECTED';
 export const FETCH_BELT_IMAGES = 'FETCH_BELT_IMAGES';
+
+// Login
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
