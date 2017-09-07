3ef6f76218bcf6b86fd37775b0c375898b3b3ab2
Scott DeVito <scottadevito@gmail.com>
2017-09-06 23:31:08 -0400

    Change admin styles and UserInfo

--- src/components/admin/user_info.js
+++ src/components/admin/user_info.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Avatar } from 'antd';
 
 const UserInfo = () => {
   return (
     <div className="user-info">
+      <Avatar size="large" icon="user" />
       <h2 className="user-name">User1@gmail.com</h2>
       <div className="user-belt">Green Belt</div>
     </div>
