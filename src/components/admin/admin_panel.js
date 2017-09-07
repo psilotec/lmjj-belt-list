0f144ed7f60b91af148ab1e6e7fed86f7e07688e
Scott DeVito <scottadevito@gmail.com>
2017-09-06 23:08:11 -0400

    Create AdminPanel

--- src/components/admin/admin_panel.js
+++ src/components/admin/admin_panel.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import AdminPanelDisplay from './admin_panel_display';
+import { Input } from 'antd';
+const Search = Input.Search;
 
 class AdminPanel extends Component {
   render() {
-    return <div>Admin Panel</div>;
+    return (
+      <div className="admin-panel">
+        <div className="user-search">
+          <Search
+            placeholder="input search text"
+            style={{ width: 200 }}
+            size="large"
+            onSearch={value => console.log(value)}
+          />
+        </div>
+
+        <AdminPanelDisplay />
+      </div>
+    );
   }
 }
 
