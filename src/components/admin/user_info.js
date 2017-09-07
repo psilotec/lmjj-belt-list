3c1bf90b5f0ec56a877b27bbdae5f8b29d3223d4
Scott DeVito <scottadevito@gmail.com>
2017-09-06 23:08:32 -0400

    Create UserInfo

---
+++ src/components/admin/user_info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserInfo = () => {
+  return (
+    <div className="user-info">
+      <h2 className="user-name">User1@gmail.com</h2>
+      <div className="user-belt">Green Belt</div>
+    </div>
+  );
+};
+
+export default UserInfo;
