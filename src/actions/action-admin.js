cfc2f4e0bcc8fb7a80d5a6070cca0fa7fa99775c
Scott DeVito <scottadevito@gmail.com>
2017-09-08 19:38:28 -0400

    Add firebase search logic to searchUser action

--- src/actions/action-admin.js
+++ src/actions/action-admin.js
@@ -1,22 +1,26 @@
-// import {} from './types';
+import { SEARCH_USER } from './types';
 import database from '../startup/db';
 
 const Users = database.ref().child('users');
 
 const searchUser = email => {
-  console.log(email);
-  // Find user
-  Users.orderByChild('name')
-    .startAt(email)
-    .endAt(email + '\uf8ff')
-    .once('value');
-  // Fetch user data
+  return dispatch => {
+    console.log(email);
+    // Find user
+    Users.orderByChild('name')
+      .startAt(email)
+      .endAt(email + '\uf8ff')
+      .once('value')
+      .then(snapshot => {
+        dispatch({
+          type: SEARCH_USER,
+          payload: snapshot.val(),
+        });
+      });
+    // Fetch user data
 
-  // Return user data object to be parsed in reducer
-  // return {
-  //   type: USER_SELECTED,
-  //   payload: belt,
-  // };
+    // Return user data object to be parsed in reducer
+  };
 };
 
 export { searchUser };
