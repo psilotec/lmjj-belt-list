a2ae44e0a29db7846f368df73e74e7055ccf223d
Scott DeVito <scottadevito@gmail.com>
2017-09-14 15:35:24 -0400

    Add user buttons to ChooseUser and style

--- src/components/admin/choose_user.js
+++ src/components/admin/choose_user.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 
-class EditUserSettings extends Component {
+class ChooseUser extends Component {
   // Map over users object and display user name
   mapUsers = () => {
     return Object.entries(this.props.searchedUser).map(user => {
-      return <div key={user[1].name}>{user[1].name}</div>;
+      return (
+        <Button key={user[1].name} className="choose-user-btn">
+          {user[1].name}
+        </Button>
+      );
     });
   };
 
   render() {
-    return <div>{this.mapUsers()}</div>;
+    return (
+      <div className="choose-user">
+        <h1>Please select a user</h1>
+        {this.mapUsers()}
+      </div>
+    );
   }
 }
 
-export default EditUserSettings;
+export default ChooseUser;
