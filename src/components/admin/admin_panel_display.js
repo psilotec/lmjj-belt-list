2abf8a81e75320bd2a89c6b710f47503e60b1c66
Scott DeVito <scottadevito@gmail.com>
2017-09-14 14:49:56 -0400

    Add condition to allow admin to select from multiple users returned by search
    If there are multiple users returned from the search query, display ChooseUser component

--- src/components/admin/admin_panel_display.js
+++ src/components/admin/admin_panel_display.js
@@ -4,7 +4,11 @@ import EditUserSettings from './edit_user_settings';
 const AdminPanelDisplay = ({ searchedUser }) => {
   return (
     <div className="admin-panel-display">
-      {searchedUser !== null ? <EditUserSettings /> : ''}
+      {searchedUser !== null ? (
+        <EditUserSettings searchedUser={searchedUser} />
+      ) : (
+        ''
+      )}
     </div>
   );
 };
--- src/components/admin/edit_user_settings.js
+++ src/components/admin/edit_user_settings.js
@@ -10,35 +10,45 @@ function handleChange(value) {
 
 class EditUserSettings extends Component {
   render() {
+    // If there are multiple users returned from the search query, display ChooseUser component
+    let multipleUsers =
+      this.props.searchedUser && Object.keys(this.props.searchedUser).length;
+
     return (
       <div>
-        <UserInfo />
-        <Select
-          defaultValue="currentBelt"
-          style={{ width: 200 }}
-          size="large"
-          onChange={handleChange}
-        >
-          <Option value="white">White</Option>
-          <Option value="yellow">Yellow</Option>
-          <Option value="yellowb">Yellow - Black stripe</Option>
-          <Option value="orange">Orange</Option>
-          <Option value="orangeb">Orange - Black Stripe</Option>
-          <Option value="green">Green</Option>
-          <Option value="greenb">Green - Black Stripe</Option>
-          <Option value="red">Red</Option>
-          <Option value="redb">Red - Black Stripe</Option>
-          <Option value="blue">Blue</Option>
-          <Option value="blueb">Blue - Black Stripe</Option>
-          <Option value="purple">Purple</Option>
-          <Option value="purpleb">Purple - Black Stripe</Option>
-          <Option value="brown">Brown</Option>
-          <Option value="brownb">Brown - Black Stripe</Option>
-          <Option value="black">Black</Option>
-        </Select>
-        <Button className="admin-submit" type="primary">
-          Submit
-        </Button>
+        {multipleUsers > 1 ? (
+          <div>Multiple Users</div>
+        ) : (
+          <div>
+            <UserInfo />
+            <Select
+              defaultValue="currentBelt"
+              style={{ width: 200 }}
+              size="large"
+              onChange={handleChange}
+            >
+              <Option value="white">White</Option>
+              <Option value="yellow">Yellow</Option>
+              <Option value="yellowb">Yellow - Black stripe</Option>
+              <Option value="orange">Orange</Option>
+              <Option value="orangeb">Orange - Black Stripe</Option>
+              <Option value="green">Green</Option>
+              <Option value="greenb">Green - Black Stripe</Option>
+              <Option value="red">Red</Option>
+              <Option value="redb">Red - Black Stripe</Option>
+              <Option value="blue">Blue</Option>
+              <Option value="blueb">Blue - Black Stripe</Option>
+              <Option value="purple">Purple</Option>
+              <Option value="purpleb">Purple - Black Stripe</Option>
+              <Option value="brown">Brown</Option>
+              <Option value="brownb">Brown - Black Stripe</Option>
+              <Option value="black">Black</Option>
+            </Select>
+            <Button className="admin-submit" type="primary">
+              Submit
+            </Button>
+          </div>
+        )}
       </div>
     );
   }
