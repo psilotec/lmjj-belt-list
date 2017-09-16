d1a27690a32472f1d70e87ff885d3760919cd846
Scott DeVito <scottadevito@gmail.com>
2017-09-15 21:45:45 -0400

    Change color of belt display according to userBeltPermission state

--- src/components/admin/user_info.js
+++ src/components/admin/user_info.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Avatar } from 'antd';
 
-const UserInfo = ({ searchedUser }) => {
-  return (
-    <div className="user-info">
-      <Avatar size="large" icon="user" />
-      <h2 className="user-name">{Object.entries(searchedUser)[0][1].name}</h2>
-      <div className="user-belt">Green Belt</div>
-    </div>
-  );
-};
+class UserInfo extends Component {
+  displayBeltColor = () => {
+    const beltColors = [
+      'white',
+      'yellow',
+      'orange',
+      'green',
+      'blue',
+      'red',
+      'purple',
+      'brown',
+      'black',
+    ];
+
+    // Return belt color that is === user's beltId
+    let found = beltColors.filter(belt => {
+      return (
+        beltColors.indexOf(belt) ===
+        Object.entries(this.props.searchedUser)[0][1].beltPermissionId
+      );
+    });
+    return found;
+  };
+
+  render() {
+    return (
+      <div className="user-info">
+        <Avatar size="large" icon="user" />
+        <h2 className="user-name">
+          {Object.entries(this.props.searchedUser)[0][1].name}
+        </h2>
+        <div
+          className={`user-belt ${this.displayBeltColor()}`}
+        >{`${this.displayBeltColor()} belt`}</div>
+      </div>
+    );
+  }
+}
 
 export default UserInfo;
